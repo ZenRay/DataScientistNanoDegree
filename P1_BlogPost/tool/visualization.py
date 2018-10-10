87861c34f0223aa216f5a4822d2477e81dca05f2
ZenRay <jake2011ren@gmail.com>
2018-10-11 07:37:31 +0800

    feat($project1 $EDA): Concate with the transform the groupby result
    use function group_for_concat to groupby a specific field to caculate the columns,
    so that update result with the groupby result

--- P1_BlogPost/tool/visualization.py
+++ P1_BlogPost/tool/visualization.py
@@ -46,7 +46,8 @@ def parse_amenity(
         return result
 
 def group_for_concat(
-    data, by, calulate_columns, func, new_field="type", by_value=None
+    data, by, calculate_columns, func, new_field="type", by_value=None, 
+    by_field="condition", melt_var_name="variable", melt_value_name="value"
 ):
     """Group by the field, prepare the data to concate
 
@@ -62,9 +63,51 @@ def group_for_concat(
         new_field: string
             A new label name, store the by value 
         by_value: string default None
-            A value put into new_field, if it's not None
+            A value put into new_field, if it's not None. Otherwise, use the by
+            as the value
+        by_field: string default condition
+            A label name is used to change the label name that is create by 
+            reset_index
+        melt_var_name: string default variable
+            A label name is used to change the label name that is a parameter 
+            var_name in melt method
+        melt_value_name: string default value
+            A label name is used to change the label name that is a parameter 
+            value_name in melt method
     Results:
         result: DataFrame
             A prepared DataFrame will be used to concate
+
+    Examples:
+    ---------
+    >>> data.groupby("amenity_internet")[["price", "reviews"]].mean() \
+    ...     .reset_index()
+    	amenity_internet   	price	    reviews
+    0	    False	    109.839506      38.416481
+    1	    True	    128.349051	    56.378976
+
+    >>> group_for_concat(
+        data, "amenity_internet", ["price", "reviews"],np.mean, 
+        by_value="internet"
+    )
+        type	condition	variable	value
+    0	internet	False	price	109.839506
+    1	internet	True	price	128.349051
+    2	internet	False	reviews	38.416481
+    3	internet	True	reviews	56.378976
     """
-            
+    data = data.copy()
+
+    if by_value is None:
+        by_value = by
+
+    result = data.groupby(by=by)[calculate_columns].apply(func).reset_index()
+
+    result = result.melt(
+        id_vars=by, value_vars=calculate_columns, var_name=melt_var_name,
+        value_name=melt_value_name
+    )
+    
+    result[new_field] = by_value
+    result.rename({by:by_field}, inplace=True, axis=1)
+    return result[[new_field, by_field, melt_var_name, melt_value_name]]
